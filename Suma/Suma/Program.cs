50b35fea6cbab22c614ed08933317e4da3bbab2b
pc8 <zaimov@gmail.com>
2024-12-13 09:36:53 +0200

    h

---
+++ Suma/Suma/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Hello, World!");
+int c, a;
+
+
+
+
+
+
+Console.Write("a=");
+double a= double.Parse(Console.ReadLine());
+Console.Write("b=");
+double b=double.Parse(Console.ReadLine());
+double c= a + b;
+Console.WriteLine("c=");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
---
+++ variant/variant/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 5;
+            while (true)
+            {
+                if (a < 10)
+                {
+                    Console.WriteLine("a = " + a);
+                    a++;
+                }
+                break;
+            }
+
+        }
+    }
+}
