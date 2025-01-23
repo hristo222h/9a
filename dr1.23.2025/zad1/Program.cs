fd3fcf6318db29fb7005f8e789c82a869acd5ee3
hristo222h <hristomirhristov09@gmail.com>
2025-01-23 07:22:46 -0800

    dr1.23.25

---
+++ dr1.23.2025/zad1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            
+           
+            Console.Write("Въведи числа, разделени с интервал: ");
+            string chisla = Console.ReadLine();
+            int[] numbers = chisla.Split(' ').Select(int.Parse).ToArray();
+            int minElement = numbers.Min();
+            int maxElement = numbers.Max();
+            int sum = numbers.Sum();
+            double average = numbers.Average();
+
+            Console.WriteLine($"Най-малкият елемент е: {minElement}");
+            Console.WriteLine($"Най-големият елемент е: {maxElement}");
+            Console.WriteLine($"Сумата на елементите е: {sum}");
+            Console.WriteLine($"Средната стойност на елементите е: {average}");
+
+        }
+    }
+}
---
+++ dr1.23.2025/zad2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("Въведи числа, разделени с интервал: ");
+            string chisla = Console.ReadLine();
+            int[] numbers = Array.ConvertAll(chisla.Split(' '), int.Parse);
+            Array.Sort(numbers);
+
+            int maxFrequency = 1;
+            int currentFrequency = 1;
+            int mostFrequentNumber = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    currentFrequency++;
+                }
+                else
+                {
+                    currentFrequency = 1;
+                }
+                if (currentFrequency > maxFrequency)
+                {
+                    maxFrequency = currentFrequency;
+                    mostFrequentNumber = numbers[i];
+                }
+
+                    
+            }
+            Console.WriteLine($"Наѝ-често срещано число е:{mostFrequentNumber}");
+        }
+    }
+}
+    
+
