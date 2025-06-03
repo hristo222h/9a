ebe1fb95cdc7c94484f9a6d691dfbc433a6087d6
hristo222h <hristomirhristov09@gmail.com>
2025-06-03 12:26:12 +0300

    Add files via upload

---
+++ PC/kucheshka sreshta/Dog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace kucheshka_sreshta
+{
+    internal class Dog
+    {
+        private string name;
+        public Dog(string name)
+        {
+            this.name = name;
+        }
+        public Dog()
+        {
+            this.name = "Balkan";
+        }
+        public string Name 
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public void Bark()
+        {
+            Console.WriteLine("kucheto kaza bal bal i negovo ime e {0}", name);
+        }
+    
+
+    }
+}
---
+++ PC/kucheshka sreshta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kucheshka_sreshta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string first = null;
+            Console.Write("Vuvedi ime za purvoto kuche: ");
+            first = Console.ReadLine();
+            Dog dog = new Dog(first);
+            Dog secound = new Dog();
+            Console.Write("Vuvedi vtoro ime: ");
+            string secound1 = Console.ReadLine();
+            secound.Name = secound1;
+            Dog malkodog1 = new Dog();
+            Dog[] dogs = new Dog[] { dog, secound, malkodog1 };
+            foreach(Dog malkodog2 in dogs)
+            {
+                malkodog2.Bark();
+            }
+
+        }
+    }
+}
