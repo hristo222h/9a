38035fe24391b24c8285fe2e9d3f46fe35631197
hristo222h <hristomirhristov09@gmail.com>
2025-06-04 13:49:44 +0300

    Add files via upload

---
+++ PC/niz email/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace niz_email
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Въведи email: ");
+            string email = Console.ReadLine();
+            int broi = email.Count();
+            int znak = email.IndexOf("@");
+            int predi = znak - 1;
+            int sled = broi - (predi + 3);
+            Console.WriteLine(predi);
+            Console.WriteLine(sled);
+
+            if (predi >= sled )
+            {
+                Console.WriteLine("Call her");
+            }
+            else
+            {
+                Console.WriteLine("She is not the one");
+            }
+
+
+
+
+
+        }
+    }
+}
